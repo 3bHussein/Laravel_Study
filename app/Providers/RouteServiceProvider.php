0539453c0b915492c171677c7e5baf856fdf232e
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-11-10 13:35:37 +0200

    /

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-                Route::middleware('web')
+                Route::middleware('admin')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/admin.php'));
         });
